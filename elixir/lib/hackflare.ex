1093d466f2be8e2fb875979e32289c285910e5f0
SeradedStripes <seradedstripes@gmail.com>
2026-04-18 18:51:07 +0200

    Elixir code init

---
+++ elixir/lib/hackflare.ex
@@ -0,0 +1,18 @@
+defmodule Hackflare do
+  @moduledoc """
+  Documentation for `Hackflare`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hackflare.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/test/hackflare_test.exs
@@ -0,0 +1,8 @@
+defmodule HackflareTest do
+  use ExUnit.Case
+  doctest Hackflare
+
+  test "greets the world" do
+    assert Hackflare.hello() == :world
+  end
+end
