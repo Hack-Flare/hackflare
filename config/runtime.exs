eac5ffaf24d92c4ca86231f02635c38b46f0998a
jam06452 <jam@jam06452.uk>
2026-04-19 14:41:31 +0100

    Ports & Elixir Version

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,7 +18,7 @@ import Config
 # script that automatically sets the env var above.
 config :hackflare, :dns, %{
   bind: System.get_env("DNS_BIND") || "0.0.0.0",
-  port: System.get_env("DNS_PORT") || 5353
+  port: System.get_env("DNS_PORT") || 53
 }
 
 if System.get_env("PHX_SERVER") do
