3ae19a9b3547c1e84e70b1ba958b61326abdd019
jam06452 <danjward8@gmail.com>
2026-04-20 22:54:45 +0100

    Dialyzer Caching

--- mix.exs
+++ mix.exs
@@ -11,7 +11,12 @@ defmodule Hackflare.MixProject do
       aliases: aliases(),
       deps: deps(),
       compilers: [:phoenix_live_view] ++ Mix.compilers(),
-      listeners: [Phoenix.CodeReloader]
+      listeners: [Phoenix.CodeReloader],
+      dialyzer: [
+        plt_add_apps: [:ex_unit],
+        plt_local_path: "priv/plts",
+        plt_core_path: "priv/plts"
+      ]
     ]
   end
 
