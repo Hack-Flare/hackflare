809b88d767dcfb46980dc5323802a186a1150f32
jam06452 <danjward8@gmail.com>
2026-04-20 21:14:43 +0100

    Tests returned pure html, made it use pat match instead of looking for exact response

--- test/hackflare_web/controllers/error_html_test.exs
+++ test/hackflare_web/controllers/error_html_test.exs
@@ -5,10 +5,12 @@ defmodule HackflareWeb.ErrorHTMLTest do
   import Phoenix.Template, only: [render_to_string: 4]
 
   test "renders 404.html" do
-    assert render_to_string(HackflareWeb.ErrorHTML, "404", "html", []) == "Not Found"
+    assert render_to_string(HackflareWeb.ErrorHTML, "404", "html", []) =~ "404"
+    assert render_to_string(HackflareWeb.ErrorHTML, "404", "html", []) =~ "Page not found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(HackflareWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(HackflareWeb.ErrorHTML, "500", "html", []) =~ "500"
+    assert render_to_string(HackflareWeb.ErrorHTML, "500", "html", []) =~ "Internal Server Error"
   end
 end
--- test/hackflare_web/controllers/page_controller_test.exs
+++ test/hackflare_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule HackflareWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "HackFlare"
+    assert html_response(conn, 200) =~ "Cloudflare alternative for HackClub"
   end
 end
