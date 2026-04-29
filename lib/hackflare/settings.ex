97d3a16c5f0d683c47b1d6d6fbc7f0c9bd4d6b73
SeradedStripes <seradedstripes@gmail.com>
2026-04-29 14:10:43 +0200

    fix type check errors

--- lib/hackflare/settings.ex
+++ lib/hackflare/settings.ex
@@ -87,18 +87,30 @@ defmodule Hackflare.Settings do
   end
 
   def runtime_overrides do
-    case Repo.one(from setting in AppSetting, where: setting.name == ^@runtime_name, limit: 1) do
-      %AppSetting{data: data} when is_map(data) -> to_atom_map(data)
-      _ -> %{}
+    try do
+      case Repo.one(from setting in AppSetting, where: setting.name == ^@runtime_name, limit: 1) do
+        %AppSetting{data: data} when is_map(data) -> to_atom_map(data)
+        _ -> %{}
+      end
+    rescue
+      Ecto.QueryError -> %{}
+      DBConnection.ConnectionError -> %{}
+      Postgrex.Error -> %{}
     end
   end
 
   def update_runtime(attrs) when is_map(attrs) do
     data = deep_merge(runtime_overrides(), to_atom_map(attrs))
 
-    %AppSetting{}
-    |> AppSetting.changeset(%{name: @runtime_name, data: data})
-    |> Repo.insert_or_update()
+    try do
+      %AppSetting{}
+      |> AppSetting.changeset(%{name: @runtime_name, data: data})
+      |> Repo.insert_or_update()
+    rescue
+      Ecto.QueryError -> {:error, :settings_table_missing}
+      DBConnection.ConnectionError -> {:error, :settings_table_missing}
+      Postgrex.Error -> {:error, :settings_table_missing}
+    end
   end
 
   def restart_nameserver do
@@ -181,8 +193,6 @@ defmodule Hackflare.Settings do
       end
     end)
   end
-
-  defp deep_merge(_left, right), do: right
 
   defp to_atom_map(%{} = map) do
     Map.new(map, fn {key, value} -> {to_atom_key(key), normalize_value(value)} end)
