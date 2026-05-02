92ff49447e4a78b4fa2f1a20fa916cadc537f83f
SeradedStripes <seradedstripes@gmail.com>
2026-05-02 17:19:53 +0200

    fix test

--- test/hackflare_web/controllers/admin_controller_test.exs
+++ test/hackflare_web/controllers/admin_controller_test.exs
@@ -4,9 +4,9 @@ defmodule HackflareWeb.AdminControllerTest do
   test "GET /admin requires authentication", %{conn: conn} do
     conn = get(conn, ~p"/admin")
 
-    assert redirected_to(conn) == "/"
+    assert redirected_to(conn) == "/auth/request"
 
     assert Phoenix.Flash.get(conn.assigns.flash, :error) ==
-             "You need to be signed in to access dash"
+             "You need to be signed in to access the dashboard"
   end
 end
