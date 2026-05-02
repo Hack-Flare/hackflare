7525d539cb49f63cec4e01f30e00b7b38dcd5861
SeradedStripes <seradedstripes@gmail.com>
2026-05-02 10:59:47 +0200

    remove redundent migration

--- priv/repo/migrations/20260501000100_add_ns_verified_to_dns_zones.exs
+++
@@ -1,9 +0,0 @@
-defmodule Hackflare.Repo.Migrations.AddNsVerifiedToDnsZones do
-  use Ecto.Migration
-
-  def change do
-    alter table(:dns_zones) do
-      add :ns_verified, :boolean, null: false, default: false
-    end
-  end
-end
