4e215e9b430d1f4e04021b202d42b3ae3a8a0e9e
SeradedStripes <seradedstripes@gmail.com>
2026-05-02 13:30:40 +0200

    add the missing flash

--- test/hackflare_web/components/layouts_test.exs
+++ test/hackflare_web/components/layouts_test.exs
@@ -6,6 +6,7 @@ defmodule HackflareWeb.LayoutsTest do
   test "domains dashboard shows re-verify action for unverified zones" do
     html =
       render_component(&HackflareWeb.Layouts.dashboard/1,
+        flash: %{},
         current_view: :domains,
         current_user: nil,
         zones: [%{name: "example.com", ns_verified: false}],
