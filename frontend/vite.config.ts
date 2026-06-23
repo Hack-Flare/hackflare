1a186ebfe6b1fbd8ca49d01120428b9c4e6b1aef
SeradedStripes <seradedstripes@gmail.com>
2026-06-23 14:33:03 +0200

    Vite now supports path resolution natively

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter() ],
   server: {
     port: 3000,
     proxy: {
       "/api": "http://localhost:8080",
     },
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
 })
