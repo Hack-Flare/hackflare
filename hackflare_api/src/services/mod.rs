9fc35e1f4319810f21888f16fa1592c4adab80d4
SeradedStripes <seradedstripes@gmail.com>
2026-09-20 18:43:39 +0200

    Del hackflare_api

--- hackflare_api/src/services/mod.rs
+++
@@ -1,7 +0,0 @@
-pub(crate) mod api_keys;
-pub(crate) mod config_overrides;
-pub(crate) mod email;
-pub(crate) mod notifications;
-pub(crate) mod password_reset;
-pub(crate) mod user_sessions;
-pub(crate) mod users;
